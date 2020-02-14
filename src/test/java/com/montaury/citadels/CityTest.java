99066f2304132d2bceee11730be6563ac2d38cfd
Dorian GIL <gildorian2000@gmail.com>
2020-02-14 10:08:43 +0100

    Ajout d'un setup et nommage correcte des tests

--- src/test/java/com/montaury/citadels/CityTest.java
+++ src/test/java/com/montaury/citadels/CityTest.java
@@ -1,35 +1,32 @@
 package com.montaury.citadels;
 
-import org.assertj.core.api.Assertions;
-
 import com.montaury.citadels.district.Card;
-import com.montaury.citadels.district.DestructibleDistrict;
-import com.montaury.citadels.district.District;
-import com.montaury.citadels.district.DistrictType;
-import com.montaury.citadels.player.Player;
-import io.vavr.collection.List;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Optional;
-
 public class CityTest {
-    @Test
-    public  void  test_Cout_Construction() {
-        Board board =  new  Board ();
-        City ville =  new  City(board);
-        Possession possession =  new  Possession ( 0 , null ); // 0 ou donc 0 score
-        ville.buildDistrict (Card.MANOR_5); // score +3
-        ville.buildDistrict (Card.WATCHTOWER_2); // +1 score
-        ville.buildDistrict (Card.TAVERN_5 ); // +1 score
-        int score = ville.score (possession);
-        Assertions.assertThat (score).isEqualTo(5);
+    City city;
+
+    @Before
+    public void setUp() {
+        Board board = new Board();
+        city = new City(board);
     }
 
     @Test
-    public void test_first_player(){
-        Board board = new Board();
-        City city = new City(board);
-        Possession possession = new Possession(0,null); // 0 or donc 0 score
+    public void construction_made() {
+        Possession possession = new Possession(0, null); // 0 ou donc 0 score
+        city.buildDistrict(Card.MANOR_5); // score +3
+        city.buildDistrict(Card.WATCHTOWER_2); // +1 score
+        city.buildDistrict(Card.TAVERN_5); // +1 score
+        int score = city.score(possession);
+        Assertions.assertThat(score).isEqualTo(5);
+    }
+
+    @Test
+    public void first_player_with_completed_city() {
+        Possession possession = new Possession(0, null); // 0 or donc 0 score
         city.buildDistrict(Card.WATCHTOWER_2); // +1 score
         city.buildDistrict(Card.MANOR_5); // +3 score
         city.buildDistrict(Card.TRADING_POST_1); // +2 score
@@ -38,22 +35,20 @@ public class CityTest {
         city.buildDistrict(Card.TOWN_HALL_2); // +5 score
         city.buildDistrict(Card.CHURCH_3); // +2 score
         int score = city.score(possession);
-        Assertions.assertThat(score).isEqualTo(23); // 19 points + 4 points -> 23 au total
+        Assertions.assertThat(score).isEqualTo(23); // 19 points + 4 points car c'est le premier joueur à avoir la cité est complétée -> 23 au total
 
     }
 
     @Test
-    public void test_district_all_types(){
-        Board board = new Board();
-        City city = new City(board);
-        Possession possession = new Possession(0,null);
-        city.buildDistrict(Card.PALACE_3); // +,NOBLE
+    public void give_bonus_points_for_all_types_districts() {
+        Possession possession = new Possession(0, null);
+        city.buildDistrict(Card.PALACE_3); // +5,NOBLE
         city.buildDistrict(Card.BATTLEFIELD_2); // +3,MILITARY
-        city.buildDistrict(Card.DOCKS_3); // +1,TRADE
+        city.buildDistrict(Card.DOCKS_3); // +3,TRADE
         city.buildDistrict(Card.CHURCH_3); // +2,RELIGIOUS
         city.buildDistrict(Card.KEEP_1); // +3,SPECIAL
         int score = city.score(possession);
-        Assertions.assertThat(score).isEqualTo(13); // Normalement : 10 + Bonus 5 types : 3 -> 13 au total
+        Assertions.assertThat(score).isEqualTo(19); // Normalement : 16 + Bonus 5 types : 3 points -> 19 au total
     }
 }
 
