cb1f7dace2328545c1769404255c0e84e7c28504
Dorian GIL <gildorian2000@gmail.com>
2020-02-14 11:02:32 +0100

    Ajout du test d'un joueur ayant terminé sa cité après le premier

--- src/test/java/com/montaury/citadels/CityTest.java
+++ src/test/java/com/montaury/citadels/CityTest.java
@@ -7,10 +7,11 @@ import org.junit.Test;
 
 public class CityTest {
     City city;
+    Board board;
 
     @Before
     public void setUp() {
-        Board board = new Board();
+        board = new Board();
         city = new City(board);
     }
 
@@ -35,8 +36,7 @@ public class CityTest {
         city.buildDistrict(Card.TOWN_HALL_2); // +5 score
         city.buildDistrict(Card.CHURCH_3); // +2 score
         int score = city.score(possession);
-        Assertions.assertThat(score).isEqualTo(23); // 19 points + 4 points car c'est le premier joueur à avoir la cité est complétée -> 23 au total
-
+        Assertions.assertThat(score).isEqualTo(23); // 19 points + 4 points car c'est le premier joueur à avoir la cité complétée -> 23 au total
     }
 
     @Test
@@ -49,6 +49,28 @@ public class CityTest {
         city.buildDistrict(Card.KEEP_1); // +3,SPECIAL
         int score = city.score(possession);
         Assertions.assertThat(score).isEqualTo(19); // Normalement : 16 + Bonus 5 types : 3 points -> 19 au total
+    }
+
+    @Test
+    public void second_player_with_completed_city() {
+        City city2 = new City(board);
+        Possession possession = new Possession(0, null); // 0 or donc 0 score
+        city.buildDistrict(Card.WATCHTOWER_2); // +1 score
+        city.buildDistrict(Card.MANOR_5); // +3 score
+        city.buildDistrict(Card.TRADING_POST_1); // +2 score
+        city.buildDistrict(Card.TAVERN_5); // +1 score
+        city.buildDistrict(Card.TOWN_HALL_1); // +5 score
+        city.buildDistrict(Card.TOWN_HALL_2); // +5 score
+        city.buildDistrict(Card.CHURCH_3); // +2 score
+        city2.buildDistrict(Card.WATCHTOWER_2); // +1 score
+        city2.buildDistrict(Card.MANOR_5); // +3 score
+        city2.buildDistrict(Card.TRADING_POST_1); // +2 score
+        city2.buildDistrict(Card.TAVERN_5); // +1 score
+        city2.buildDistrict(Card.TOWN_HALL_1); // +5 score
+        city2.buildDistrict(Card.TOWN_HALL_2); // +5 score
+        city2.buildDistrict(Card.CHURCH_3); // +2 score
+        int score = city2.score(possession);
+        Assertions.assertThat(score).isEqualTo(21); // 19 points + 2 points car c'est le second joueur à avoir la cité complétée -> 21 au total
     }
 }
 
